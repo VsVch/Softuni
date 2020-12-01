be01e36afaa79faf070996fe08a842916dc518bf
Sand82 <sandoki@abv.bg>
2020-12-01 17:11:07 +0200

    All my exercises from softuni basics course

---
+++ Programming-Basics/For Loop-Lab/09.LeftandRightSum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Xml.Schema;
+
+namespace _09.LeftandRightSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int leftSum = 0;
+            int rightSum = 0;
+            
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                leftSum += number;                                                          
+            }
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                rightSum += number;
+            }
+            if (rightSum == leftSum)
+            {
+                Console.WriteLine($"Yes, sum = {leftSum}");
+            }
+            else
+            {
+                int totalSum = rightSum - leftSum;
+                Console.WriteLine($"No, diff = {Math.Abs(totalSum)}");
+            }
+            
+            
+
+        }
+    }
+}
---
+++ Programming-Basics/For Loop-Lab/10.OddEvenSum/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _10.OddEvenSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int evenSum = 0;
+            int oddSum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int inputNumber = int.Parse(Console.ReadLine());
+                if (i % 2 == 0)
+                {
+                    evenSum += inputNumber;
+                    
+                }
+                else
+                {
+                     oddSum += inputNumber;
+                    
+                }
+                
+            }
+            
+            if (evenSum == oddSum)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {evenSum}");
+            }
+            else
+            {
+                int totallSum = evenSum - oddSum;
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(totallSum)}");
+            }
+            
+        }
+    }
+}
