a4e856073640a3a041ae4dee4181e7719af32635
Sand82 <sandoki@abv.bg>
2020-12-01 17:10:25 +0200

    All my exercises from softuni basics course

---
+++ Programming-Basics/For LoopExercise/02.HalfSumElement/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _02.HalfSumElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int maxNumber =  int.MinValue;
+            
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                sum += number;
+                int sumLowNumbers = number - maxNumber;
+                if (number > maxNumber)
+                {
+                    maxNumber = number;
+                }
+            }   
+            int sumWhitoutMax = sum - maxNumber;
+            
+            if (sumWhitoutMax == maxNumber)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {maxNumber}");
+            }
+            else
+            {
+                int diference = sumWhitoutMax - maxNumber;
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(diference)}");
+            }
+        }
+    }
+}
---
+++ Programming-Basics/For LoopExercise/05.DivideWithoutRemainder/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Xml.Schema;
+
+namespace _05.DivideWithoutRemainder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            const double doubleConverter = 1.0;
+            int p1 = 0;
+            int p2 = 0;
+            int p3 = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (number % 2 == 0)
+                {
+                    p1++;
+                }
+                if (number % 3 == 0)
+                {
+                    p2++;
+                }
+                if (number % 4 == 0)
+                {
+                    p3++;
+                }
+
+            }
+            double pp1 = p1 * doubleConverter * 100 / n;
+            double pp2 = p2 * doubleConverter * 100 / n;
+            double pp3 = p3 * doubleConverter * 100 / n;
+            Console.WriteLine($"{pp1:f2}%");
+            Console.WriteLine($"{pp2:f2}%");
+            Console.WriteLine($"{pp3:f2}%");
+                
+        }
+    }
+}
