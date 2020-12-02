ffc5f315d278eab912fad9cd5a892d0542c78eef
Sand82 <sandoki@abv.bg>
2020-12-02 17:07:12 +0200

    add new exercises from softuni fundamentals course

---
+++ Programming-Fundamentals-C#/Regular Expressions-More Exercise/3.PostOffice/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _3.PostOffice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split("|");
+
+            string firstPartPattern = @"([#$%.&])([A-Z]+)\1";
+
+            Regex regexOne = new Regex(firstPartPattern);
+
+            Match matchOne = regexOne.Match(input[0]);
+
+            if (matchOne.Success)
+            {
+                string capitalsWord = matchOne.Groups[2].Value;
+
+                string secondPartPattern = @"([0-9][0-9]):([0-9][0-9])";
+
+                Regex regexTwo = new Regex(secondPartPattern);
+
+                MatchCollection machesTwo = regexTwo.Matches(input[1]);
+
+                Dictionary<char, int> wordsCollections = new Dictionary<char, int>();
+
+                for (int i = 0; i < capitalsWord.Length; i++)
+                {
+                    int symbol = capitalsWord[i];
+
+                    for (int j = 0; j <machesTwo.Count; j++)
+                    {
+                        int letter = int.Parse(machesTwo[j].Groups[1].Value);
+                        int letterLenght = int.Parse(machesTwo[j].Groups[2].Value);
+
+                        if (symbol == letter)
+                        {
+                            wordsCollections.Add((char)symbol, letterLenght +1);
+                            break;
+                            
+                        }
+                    }
+                }
+                string[] text = input[2].Split(" ",StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var word in wordsCollections)
+                {
+                    char wordChar = word.Key;
+                    int wordCharLenght = word.Value;
+
+                    for (int i = 0; i < text.Length; i++)
+                    {
+                        if (text[i].Contains(wordChar) && text[i].Length == wordCharLenght)
+                        {
+                            string theWord = text[i];
+                            Console.WriteLine(theWord);
+                            break;
+                        }
+                    }
+                  
+                }
+            }
+        }
+    }
+}
---
+++ Programming-Fundamentals-C#/Regular Expressions-More Exercise/4.Santa'sSecretHelper/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _4.Santa_sSecretHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = int.Parse(Console.ReadLine());
+
+            string input;            
+
+            string pattern = @"@([A-Z][a-z]+)[^@\-!:>]*!([A-Z])!";
+
+            Regex regex = new Regex(pattern);
+
+            List<string> names = new List<string>();
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string criptedMesseg = string.Empty;
+
+                for (int i = 0; i < input.Length; i++)
+                {
+                    int newDigit = input[i] - key;
+
+                    char letter = (char)newDigit;
+
+                    criptedMesseg += letter;                    
+                }
+
+                Match match = regex.Match(criptedMesseg);
+
+                if (match.Success)
+                {
+                    string name = match.Groups[1].Value;
+
+                    char behavior = char.Parse(match.Groups[2].Value);
+
+                    if (behavior == 'N')
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        names.Add(name);
+                    }                    
+                }
+            }
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
