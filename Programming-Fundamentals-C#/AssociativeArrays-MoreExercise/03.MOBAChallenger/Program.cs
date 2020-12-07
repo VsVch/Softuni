d655658979ad8427c89826dc2295d815890e2376
Aleksandar Stefanov <75274983+Sand82@users.noreply.github.com>
2020-12-07 12:15:06 +0200

    Update Program.cs

--- Programming-Fundamentals-C#/AssociativeArrays-MoreExercise/03.MOBAChallenger/Program.cs
+++ Programming-Fundamentals-C#/AssociativeArrays-MoreExercise/03.MOBAChallenger/Program.cs
@@ -1,79 +1,96 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-
-namespace _03.MOBAChallenger
+ 
+namespace MOBAChallenger
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string[] separator = new string[] {"->","vs"," "};
-
-            string input;
-
-            Dictionary<string, Dictionary<string, int>> playersInfo 
-                = new Dictionary<string, Dictionary<string, int>>();
-
-            while ((input = Console.ReadLine())!= "Season end")
+            var palyerPoll = new SortedDictionary<string, Dictionary<string, int>>(); // пазим сортиран играч, позиция и скил
+ 
+            while (true)
             {
-                List<string> playerInfo = input
-                    .Split(separator, StringSplitOptions.RemoveEmptyEntries)
-                    .ToList();
-
-                if (playerInfo.Count > 2)
+                string input = Console.ReadLine();
+ 
+                if (input.Equals("Season end"))
                 {
-                    string name = playerInfo[0];
-                    string position = playerInfo[1];
-                    int skills = int.Parse(playerInfo[2]);                     
-                                       
-                    if (!playersInfo.ContainsKey(position))
-                    {
-                        playersInfo[position] = new Dictionary<string, int>();                       
-                        playersInfo[position][name] = skills;
-                    }
-                    else
-                    {
-                        if (!playersInfo[position].ContainsKey(name))
-                        {
-                            playersInfo[position][name] = skills;
-                            playersInfo[position][] += skills;
-                            
-                        }
-                        else
-                        {
-                            int currentSkills = playersInfo[position][name];
-
-                            if (currentSkills < skills)
-                            {
-                                playersInfo[position][name] = skills;
-                            }
-                        }
-                    }
-                    
-                }                
-                else 
+                    break;
+                }
+ 
+                if (input.Contains(" -> "))
                 {
-
-                }         
+                    string[] command = input.Split(" -> ");
+                    string player = command[0];
+                    string position = command[1];
+                    int skill = int.Parse(command[2]);
+ 
+                    if (!palyerPoll.ContainsKey(player))
+                    {
+                        palyerPoll.Add(player, new Dictionary<string, int>());
+                    }
+ 
+                    if (!palyerPoll[player].ContainsKey(position))
+                    {
+                        palyerPoll[player].Add(position, 0);
+                    }
+ 
+                    if (palyerPoll[player][position] <= skill)
+                    {
+                        palyerPoll[player][position] = skill;
+                    }
+                }
+                else
+                {
+                    string[] PlayerVSplayer = input.Split(" vs ");
+ 
+                    string firstPlayer = PlayerVSplayer[0];
+                    string secondPlayer = PlayerVSplayer[1];
+ 
+                    if (!palyerPoll.ContainsKey(firstPlayer) || !palyerPoll.ContainsKey(secondPlayer))
+                    {
+                        continue;
+                    }
+ 
+                    bool commonPosition = false;
+ 
+                    foreach (var item in palyerPoll[firstPlayer])
+                    {
+                        if (palyerPoll[secondPlayer].ContainsKey(item.Key))
+                        {
+                            commonPosition = true;
+                        }
+                    }
+ 
+                    int totalSkills1 = 0;
+                    int totalSkills2 = 0;
+ 
+                    if (commonPosition)
+                    {
+                        totalSkills1 = palyerPoll[firstPlayer].Values.Sum();
+                        totalSkills2 = palyerPoll[secondPlayer].Values.Sum();
+ 
+                        if (totalSkills1 > totalSkills2)
+                        {
+                            palyerPoll.Remove(secondPlayer);
+                        }
+                        else if (totalSkills2 > totalSkills1)
+                        {
+                            palyerPoll.Remove(firstPlayer);
+                        }
+                    }
+                }
             }
-
-            //"{player}: {totalSkill} skill"
-            //"- {position} <::> {skill}"
-
-            foreach (var item in playersInfo)
+ 
+            foreach (var item in palyerPoll.OrderByDescending(x => x.Value.Values.Sum()).ThenBy(c => c.Key))
             {
-                foreach (var kvp in item.Value.OrderByDescending(v =>v.Value).ThenBy(k=>k.Key))
+                Console.WriteLine($"{item.Key}: {item.Value.Values.Sum()} skill");
+ 
+                foreach (var men in item.Value.OrderByDescending(x => x.Value).ThenBy(c => c.Key))
                 {
-                    Console.WriteLine($"{kvp.Key}: {kvp.Value} skill");
-
-                    foreach (var lvp in playersInfo.OrderByDescending(k=>k.Key).ThenBy(v=>v.Value))
-                    {
-                        Console.WriteLine($"- {lvp.Key} <::> {kvp.Value}");
-                    }
-                    
-                }   
-                
+                    Console.WriteLine($"- {men.Key} <::> {men.Value}");
+                }
             }
         }
     }
