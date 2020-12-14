b2509a1558821fa28c355d658ee5aab280f67bc8
Sand82 <sandoki@abv.bg>
2020-12-14 19:03:15 +0200

    add new practice

---
+++ Programming-Fundamentals-C#/Basic Syntax,ConditionalStatementsandLoops-Exercise/04.PrintAndSum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _04.PrintAndSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startIndex = int.Parse(Console.ReadLine());
+            int endIndex = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int i = startIndex; i <= endIndex; i++)
+            {
+                sum += i;
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
