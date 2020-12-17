bb32e060dcb242375bce85a9d4a5a427f6a7892d
Sand82 <sandoki@abv.bg>
2020-12-17 22:33:33 +0200

    add new practice

---
+++ Programming-Fundamentals-C#/Arrays-MoreExercise/03.RecursiveFibonacci/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+	string result1 = Lowercase1("Test");
+	string result2 = Lowercase2("Test"); // Call Lowercase2.
+	string result3 = Lowercase2("Test"); // Call Lowercase2 again.
+	Console.WriteLine("{0} {1} {2}", result1, result2, result3);
+    }
+
+    static string Lowercase1(string value)
+    {
+	return value.ToLower();
+    }
+
+    static Dictionary<string, string> _lowercase = new Dictionary<string, string>();
+    static string Lowercase2(string value)
+    {
+	string lookup;
+	if (_lowercase.TryGetValue(value, out lookup))
+	    return lookup;
+
+	lookup = value.ToLower();
+
+	_lowercase[value] = lookup;
+	return lookup;
+    }
+}
---
+++ Programming-Fundamentals-C#/Arrays-MoreExercise/04.FoldAndSum/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.FoldAndSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int[] array = input
+                               .Split(" ")
+                               .Select(int.Parse)
+                               .ToArray();
+            int leftCount = array.Length / 4;
+            int rightCount = array.Length;
+            int count = 0;
+
+
+            List<int> collection = new List<int>();
+
+            for (int i = (array.Length / 4) - 1; i >= 0; i--)
+            {
+                int leftSum = 0;
+
+                
+                for (int j = leftCount; j < array.Length / 2; j++)
+                {
+                    leftSum = array[i] + array[j];
+                    collection.Add(leftSum);
+                    leftCount++;
+                    break;
+                }
+            }
+           
+            for (int i = array.Length / 2; i < array.Length; i++)
+            {
+                int rightSum = 0;
+                count++;
+
+                if (array.Length * 3 / 4  == i)
+                {
+                    break;
+                }
+
+                for (int j = rightCount - 1; j >= array.Length / 2; j--)
+                {
+                    rightSum = array[i] + array[j];
+                    collection.Add(rightSum);
+                    rightCount--;
+                    break;
+                }            
+            }
+            
+            Console.Write(string.Join(" ",collection));
+            
+        }
+    }
+}
