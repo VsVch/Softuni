e2134a206edba6c537e2d65516f4bcc7872c6b2e
Sand82 <sandoki@abv.bg>
2021-01-13 20:57:10 +0200

    add new practice

---
+++ Programming-Advanced-C#/StacksAndQueues-Lab/7.HotPotato/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _7.HotPotato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] kids = Console.ReadLine()
+                                   .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            int cycle = int.Parse(Console.ReadLine());
+;
+            Queue<string> hotPotato = new Queue<string>(kids);
+
+            int counter = 0;
+
+            string kid = string.Empty;
+
+            while (hotPotato.Count > 1)
+            {
+                counter++;
+                kid = hotPotato.Peek();
+                hotPotato.Dequeue();
+
+                if (cycle == counter)
+                {
+                    counter = 0;
+                    Console.WriteLine($"Removed {kid}");
+                    
+                }
+                else
+                {
+                    hotPotato.Enqueue(kid);
+                }
+            }
+            Console.WriteLine($"Last is {hotPotato.Dequeue()}");
+        }
+    }
+}
