0fdf42c7d7d4c62097d0302fbbc1ac35ade19f5b
Sand82 <sandoki@abv.bg>
2021-01-13 21:13:26 +0200

    add new practice

---
+++ Programming-Advanced-C#/StacksAndQueues-Lab/8.TrafficJam/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _8.TrafficJam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Queue<string> cars = new Queue<string>();
+
+            string input;
+
+            int counter = 0;
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                if (input == "green")
+                {
+                    string car = string.Empty;
+
+                    for (int i = 0; i < n; i++)
+                    {
+                        if (cars.Count == 0)
+                        {
+                            break;
+                        }
+                        counter++;
+                        car = cars.Peek();
+                        cars.Dequeue();
+                        Console.WriteLine($"{car} passed!");
+                    }
+                }
+                else
+                {
+                    cars.Enqueue(input);
+                }
+            }
+            Console.WriteLine($"{counter} cars passed the crossroads.");
+        }
+    }
+}
