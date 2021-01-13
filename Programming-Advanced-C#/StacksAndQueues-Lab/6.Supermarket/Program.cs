8c9e10f7de1d21d55ebe890268ba34c7e66f9b54
Sand82 <sandoki@abv.bg>
2021-01-13 20:41:20 +0200

    add new practice

---
+++ Programming-Advanced-C#/StacksAndQueues-Lab/6.Supermarket/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _6.Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+
+            Queue<string> names = new Queue<string>();
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+
+                if (input == "Paid")
+                {
+                    while (names.Count > 0)
+                    {
+                        Console.WriteLine(names.Peek());
+                        names.Dequeue();
+                    }
+                }
+                else
+                {
+                    names.Enqueue(input);
+                }          
+               
+            }
+            Console.WriteLine($"{names.Count} people remaining.");
+        }
+    }
+}
