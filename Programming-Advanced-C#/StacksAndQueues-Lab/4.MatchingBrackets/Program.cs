471452d5ac91206a40dae11f5c9087863e9ec0cd
Sand82 <sandoki@abv.bg>
2021-01-13 20:17:18 +0200

    add new practice

---
+++ Programming-Advanced-C#/StacksAndQueues-Lab/4.MatchingBrackets/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4.MatchingBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 + (2 - (2 + 3) * 4 / (3 + 1)) * 5
+
+            string input = Console.ReadLine();
+
+            Stack<int> expression = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    expression.Push(i);
+                }
+
+                if (input[i] == ')')
+                {
+                    int startIndex = expression.Pop();
+
+                    Console.WriteLine(input.Substring(startIndex, i - startIndex + 1));
+                }
+            }
+        }
+    }
+}
