f512939fe5c7c0ecc69f1ff835fb3fba11f4d3e4
Sand82 <sandoki@abv.bg>
2021-01-16 12:12:31 +0200

    add new exercise

---
+++ Programming-Advanced-C#/StacksАndQueues-Exercise/05.FashionBoutique/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.FashionBoutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                                 .Split(" ")
+                                 .Select(int.Parse)
+                                 .ToArray();
+
+            int rackCapacity = int.Parse(Console.ReadLine());
+
+            int inUseRackCapacity = rackCapacity;
+
+            Stack<int> clotes = new Stack<int>(input);
+
+            int racksCounter = 1;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int clote = clotes.Peek();
+                inUseRackCapacity -= clote;
+
+                if (inUseRackCapacity < 0)
+                {                    
+                    inUseRackCapacity = rackCapacity;
+                    racksCounter++;
+                    inUseRackCapacity -= clote;
+                }
+
+                clotes.Pop();
+            }
+            Console.WriteLine(racksCounter);
+
+        }
+    }
+}
