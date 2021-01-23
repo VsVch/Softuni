cf5da345719a2dcd06b4477e239514df5111f667
Sand82 <sandoki@abv.bg>
2021-01-23 20:03:25 +0200

    add new exercises

---
+++ Programming-Fundamentals-C#/AssociativeArrays-MoreExercise/04.Snowwhite/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.Snowwhite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, double>> dwarfsList = new Dictionary<string, Dictionary<string, double>>();
+
+            string input;
+
+            while ((input = Console.ReadLine()) != "Once upon a time") // {dwarfName} <:> {dwarfHatColor} <:> {dwarfPhysics}
+            {
+                string[] tokens = input.Split(" <:> ");
+
+                string dwarfName = tokens[0];
+                string dwarfHatColor = tokens[1];
+                double dwarfPhysics = double.Parse(tokens[2]);
+
+                if (!dwarfsList.ContainsKey(dwarfName))
+                {
+                    dwarfsList.Add(dwarfName, new Dictionary<string, double>());
+                }
+                if (!dwarfsList[dwarfName].ContainsKey(dwarfHatColor))
+                {
+                    dwarfsList[dwarfName].Add(dwarfHatColor, dwarfPhysics);
+                }
+
+                if (dwarfsList[dwarfName][dwarfHatColor] < dwarfPhysics)
+                {
+                    dwarfsList[dwarfName][dwarfHatColor] = dwarfPhysics;
+                }
+            }        
+                
+
+            foreach (var dwarf in dwarfsList.OrderByDescending(x => x.Value.Values.Max()).ThenByDescending(x => x.Value.Count())) // (Yellow) Sasho <-> 4500
+            {
+                foreach (var item in dwarf.Value)
+                {
+                    Console.WriteLine($"({item.Key}) {dwarf.Key} <-> {item.Value}");
+                }
+            }
+        }
+    }
+}
