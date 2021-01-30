e1d89f246e9dc06974358c564f2a5a7e52d26355
Sand82 <sandoki@abv.bg>
2021-01-30 10:06:32 +0200

    add new practice

---
+++ Programming-Advanced-C#/FunctionalProgramming-Lab/FunctionalProgramming/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace FunctionalProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "5, 2, 7, 9, 8" ;
+
+            int[] array = input
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                //.Select(x => int.Parse(x))
+                .Select(Parser)
+                .ToArray();
+
+
+           array =  array.OrderBy(n => n).ToArray();
+           array =  array.OrderBy(Sort).ToArray();
+
+            Console.WriteLine(string.Join(" ", array));
+
+
+        }
+        static int Parser(string n) 
+        {
+            return int.Parse(n);
+        
+        }
+
+
+
+        static int Sort(int n) 
+        {
+            return n;
+        
+        }
+    }
+}
