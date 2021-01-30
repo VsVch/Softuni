9306511cb6275ae8227438fceb8609515bc2a560
Sand82 <sandoki@abv.bg>
2021-01-30 17:54:18 +0200

    add new exercise

---
+++ Programming-Advanced-C#/FunctionalProgramming-Lab/04.AddVAT/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.AddVAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal[] numbers = Console.ReadLine()
+                .Split(", ")
+                .Select(decimal.Parse)
+                .Select(x => x * 120 / 100)
+                .ToArray();
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine($"{number:f2}");
+            }
+        }    
+   
+    }
+
+}
