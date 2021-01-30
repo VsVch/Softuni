a71deeb7c2cda5c94fe8a7e17342a5dad8f66160
Sand82 <sandoki@abv.bg>
2021-01-30 17:21:55 +0200

    add new exercise

---
+++ Programming-Advanced-C#/FunctionalProgramming-Lab/02.SumNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           //Func<string, int> parser = x=> int.Parse(x);
+
+            PintSumAndCount(int.Parse, a => a.Length,
+                
+                Array => Array.Sum());
+
+
+
+        }
+        static void PintSumAndCount(Func<string, int> parser,
+        Func<int[], int> countGetter,
+        Func<int[], int> sumCalculator)
+        {
+            int[] array = Console.ReadLine()
+                    .Split(", ")
+                    .Select(parser)
+                    .ToArray();
+
+            Console.WriteLine(countGetter(array));
+            Console.WriteLine(sumCalculator(array));
+        }
+    }         
+}
