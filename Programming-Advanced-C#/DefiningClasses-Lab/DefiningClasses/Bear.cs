1ffe1397ba1a9ead6aecb57d4dc946dc16816107
Sand82 <sandoki@abv.bg>
2021-02-01 22:22:23 +0200

    add new exercise

---
+++ Programming-Advanced-C#/DefiningClasses-Lab/DefiningClasses/Bear.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    class Bear
+    {
+        // data
+        public string Name { get; set; }
+
+        public int DaysSinceEaten { get; set; }
+
+        public int Age { get; set; }
+
+        //Behaviour
+
+        public void Eat() 
+        {
+            Console.WriteLine($"The bear {this.Name} just ate and is hppy");
+                    
+        }
+    }
+}
---
+++ Programming-Advanced-C#/DefiningClasses-Lab/DefiningClasses/Shirt.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    class Shirt
+    {
+        //private string size;
+        //private decimal price;
+        //private int quantity;
+
+        public string Size { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        public void Wash()
+        {
+            Console.WriteLine("Tshirt was dirty now is clean :) MR proper");
+        
+        }
+    }
+}
