b54de6937ec4d0dab82921dc0ee1d4a201afc526
Sand82 <sandoki@abv.bg>
2021-02-01 22:21:04 +0200

    add new practice

---
+++ Programming-Advanced-C#/DefiningClasses-Lab/StaticClasses/SoftUniMaths.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticClasses
+{
+    static class SoftUniMaths
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;        
+        }
+        public static int Muitiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+
+    
+}
