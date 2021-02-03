18eb899455fa659640cf03d9c6fd062414abf320
Sand82 <sandoki@abv.bg>
2021-02-03 20:05:46 +0200

    add new exercise

---
+++ Programming-Advanced-C#/DefiningClasses-Exercise/03.OldestFamilyMember/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        private string name;
+
+        private int age;
+        public Person()
+        {
+            name = "no name";
+            age = 1;
+        }
+
+        public Person(int age) :this()
+        {
+            this.age = age;
+        }       
+
+        public Person(string name, int age) :this(age)
+        {
+
+            this.name = name;
+            
+        }
+        public string Name 
+        {
+            get => this.name;
+            set=> this.name = value; 
+        }
+        public int Age 
+        { 
+            get => this.age; 
+            set => this.age = value; 
+        }
+    }
+}
