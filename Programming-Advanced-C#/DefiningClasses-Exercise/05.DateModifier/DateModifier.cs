f4ec6f7fe51ca8ced8503c34bef6f716f485c6c1
Sand82 <sandoki@abv.bg>
2021-02-03 20:06:55 +0200

    add new practice

---
+++ Programming-Advanced-C#/DefiningClasses-Exercise/05.DateModifier/DateModifier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _05.DateModifierProblem
+{
+    public static class DateModifier
+    {
+        public static int GetDefferencBetweenDateInDays(string dateOneStr, string dateTwoStr)
+        {
+            DateTime dateOne = DateTime.Parse(dateOneStr);
+            DateTime dateTwo = DateTime.Parse(dateTwoStr);
+            TimeSpan diff = dateOne - dateTwo;
+
+            return Math.Abs(diff.Days);        
+        }
+    }
+}
---
+++ Programming-Advanced-C#/DefiningClasses-Exercise/05.DateModifier/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _05.DateModifierProblem
+{
+    public class DateModifierproblem
+    {
+        static void Main(string[] args)
+        {
+            
+            string dateOne = Console.ReadLine();
+            string dateTwo = Console.ReadLine();
+
+            int days = DateModifier.GetDefferencBetweenDateInDays(dateOne , dateTwo);
+
+            Console.WriteLine(days);
+        }
+    }
+}
