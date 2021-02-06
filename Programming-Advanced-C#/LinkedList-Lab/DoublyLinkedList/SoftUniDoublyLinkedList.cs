e8b39adb886f49de2c1c289b096530e44eb04f41
Sand82 <sandoki@abv.bg>
2021-02-06 22:04:16 +0200

    add new exercise

---
+++ Programming-Advanced-C#/LinkedList-Lab/DoublyLinkedList/SoftUniDoublyLinkedList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoublyLinkedList
+{
+    class SoftUniDoublyLinkedList
+    {
+        public Node Head { get; set; }
+
+        public Node Tail { get; set; }
+
+        public void AddHead(Node node) 
+        {
+            if (Head == null)
+            {
+                Head = node;
+                Tail = node;
+                return;
+            }
+            node.Next = Head;
+            Head.Previus = node;
+            Head = node;
+        
+        
+        }
+    }
+}
