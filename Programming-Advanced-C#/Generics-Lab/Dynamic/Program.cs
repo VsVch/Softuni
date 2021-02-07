e66e14959249b4aa56621b76fd509630e3b72bde
Sand82 <sandoki@abv.bg>
2021-02-07 22:59:43 +0200

    ad new exercise

---
+++ Programming-Advanced-C#/Generics-Lab/Dynamic/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Dynamic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Print(10);
+            Print("String");
+        }
+        public static dynamic Print(dynamic element) 
+        {
+            Console.WriteLine(element);
+
+            return 5;
+        
+        }
+    }
+}
