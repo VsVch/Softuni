2e2cde4f3150e9f41720b7c9f730e3a09c2ccadf
Sand82 <sandoki@abv.bg>
2021-02-08 06:34:31 +0200

    add new practice

---
+++ Programming-Advanced-C#/Generics-Lab/GenericMethods/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GenericMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintKeyValuePair<int, string>(5, "Gogi");
+        }
+        static void PrintKeyValuePair<TKey, TValue>(TKey key, TValue value) 
+        {
+            Console.WriteLine($"Key: {key} Value : {value}");      
+        
+        }
+    }
+}
