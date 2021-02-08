41e32f33e92651e98d6acfc5f308df8fb094c5bb
Sand82 <sandoki@abv.bg>
2021-02-08 06:32:47 +0200

    ad new exercise

---
+++ Programming-Advanced-C#/Generics-Lab/03.GenericScale/EqualityScale.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericScale
+{
+   public class EqualityScale<T1, T2>
+    where T1 : IComparable where T2 : IComparable
+    {      
+        
+        public bool AreEqual(T1 left, T2 right)         
+        {
+
+            int result = left.CompareTo(right);
+
+            if (result == 0)
+            {
+                return true;
+            }            
+            else
+            {
+                return false;
+            }           
+        
+        
+        }
+    }
+}
---
+++ Programming-Advanced-C#/Generics-Lab/03.GenericScale/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            EqualityScale<int, int> comperor =
+                new EqualityScale<int, int>();
+
+            Console.WriteLine(comperor.AreEqual(6, 6));          
+
+        }
+    }
+}
