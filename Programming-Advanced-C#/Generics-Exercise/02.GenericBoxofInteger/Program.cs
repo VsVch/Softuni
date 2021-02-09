01dc7418db53dad1326056318a8d4d092665c443
Sand82 <sandoki@abv.bg>
2021-02-09 21:44:03 +0200

    add new practice

---
+++ Programming-Advanced-C#/Generics-Exercise/02.GenericBoxofInteger/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _02.GenericBoxofInteger
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int intigerValue = int.Parse(Console.ReadLine());
+
+                Box<int> box = new Box<int>(intigerValue);
+
+                Console.WriteLine(box.ToString());
+            }
+        }
+    }
+}
