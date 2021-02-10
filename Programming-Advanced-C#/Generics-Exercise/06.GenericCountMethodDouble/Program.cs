9b69a77082ea16ea6e12c6245b2fd2bb98697884
Sand82 <sandoki@abv.bg>
2021-02-10 21:40:18 +0200

    add new exercise

---
+++ Programming-Advanced-C#/Generics-Exercise/06.GenericCountMethodDouble/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.GenericCountMethodDouble
+{
+    public  class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<double> doubleNumbers = new List<double>();
+
+            for (int i = 0; i < n; i++)
+            {
+                double number = double.Parse(Console.ReadLine());
+                doubleNumbers.Add(number);
+            }
+
+            double element = double.Parse(Console.ReadLine());
+
+            Box<double> box = new Box<double>(doubleNumbers);
+
+            int countOfElements = box.CountOfElement(doubleNumbers, element);
+
+            Console.WriteLine(countOfElements);
+
+        }
+    }
+}
