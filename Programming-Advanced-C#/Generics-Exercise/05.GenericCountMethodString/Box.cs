827552225fbd9e229064f91b48c316abbd7b3f50
Sand82 <sandoki@abv.bg>
2021-02-10 21:39:45 +0200

    add new exercise

---
+++ Programming-Advanced-C#/Generics-Exercise/05.GenericCountMethodString/Box.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _05.GenericCountMethodString
+{
+    class Box<T> where T : IComparable
+    {       
+
+        public Box(List<T> list)
+        {
+            this.List = list;
+        }
+
+        public List<T> List { get; set; }
+
+        public int CountOfGreaterElements ( T element) 
+        {
+            int greaterElementsCounter = 0;
+
+            for (int i = 0; i < this.List.Count; i++)
+            {
+                if (element.CompareTo(List[i]) < 0)
+                {
+                    greaterElementsCounter++;
+                }
+            }
+
+            return greaterElementsCounter;
+        }
+       
+    }
+}
---
+++ Programming-Advanced-C#/Generics-Exercise/05.GenericCountMethodString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.GenericCountMethodString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> boxes = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string element = Console.ReadLine();
+
+                boxes.Add(element);
+            }
+            string elementToCompare = Console.ReadLine();
+
+            Box<string> box = new Box<string>(boxes);           
+
+            Console.WriteLine(box.CountOfGreaterElements(elementToCompare));
+        }
+    }
+}
