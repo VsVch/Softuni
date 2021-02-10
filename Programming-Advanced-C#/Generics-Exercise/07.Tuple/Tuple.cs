95c641011b930019134c39ef0f4b029429895a11
Sand82 <sandoki@abv.bg>
2021-02-10 21:40:57 +0200

    add new exercise

---
+++ Programming-Advanced-C#/Generics-Exercise/07.Tuple/Tuple.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07.Tuple
+{
+    class Tuple<T1,T2>
+    {
+        public Tuple(T1 itemOne, T2 itemTwo)
+        {
+            this.ItemOne = itemOne;
+            this.ItemTwo = itemTwo;
+        }
+
+        public T1 ItemOne { get; set; }
+
+        public T2 ItemTwo { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.ItemOne} -> {this.ItemTwo}";
+        }
+    }
+
+}
