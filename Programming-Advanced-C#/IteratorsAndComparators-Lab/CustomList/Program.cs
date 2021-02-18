2775a23832c30ac40e14fe3acf8b59a260130bd8
Sand82 <sandoki@abv.bg>
2021-02-18 19:53:23 +0200

    add new exercise

---
+++ Programming-Advanced-C#/IteratorsAndComparators-Lab/CustomList/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CustomList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomList<int> list = new CustomList<int>();
+
+            list.Add(5);
+            list.Add(3);
+
+            foreach (var item in list)
+            {
+                Console.WriteLine("In the foreach");
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
