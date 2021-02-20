32183e72c3752803a5111d75b4f0b540f6ccb96a
Sand82 <sandoki@abv.bg>
2021-02-20 22:17:38 +0200

    add new exercise

---
+++ Programming-Advanced-C#/IteratorsAndComparators-Lab/Params/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Params
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintNames("Lubcho", "Misho", "Mariq", "Sasho");
+        }
+        static void PrintNames(params string[] names) 
+        {           
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }       
+        
+        }
+    }
+}
