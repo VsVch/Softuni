ec4c0c315575c4e241b68aca6a77029e262c4858
Sand82 <sandoki@abv.bg>
2021-02-20 22:18:04 +0200

    add new exercise

---
+++ Programming-Advanced-C#/IteratorsAndComparators-Lab/YieldReturn/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace YieldReturn
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //foreach (var name in GetNames())
+            //{
+            //    Console.WriteLine(name);
+            //    Console.WriteLine("In the foreach");
+            //}
+            IEnumerator enumerator = GetNames().GetEnumerator();
+
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current);
+            }
+        }
+        public static IEnumerable<string> GetNames() 
+        {
+            yield return "Goshka";
+            yield return "Gogi";
+            Console.WriteLine("Sled Gogi sum");
+            yield return "Dimitrichko";
+        }
+    }
+}
