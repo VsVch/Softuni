6b7c5cb62d4315ed4a5214d520c3b98d1be894c1
Sand82 <sandoki@abv.bg>
2021-02-20 22:16:58 +0200

    add new exercise

---
+++ Programming-Advanced-C#/IteratorsAndComparators-Lab/Iterators/Game.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Iterators
+{
+    public class Game : IPrintable, IEnumerable
+    {
+        public void End(bool isOver)
+        {
+            Console.WriteLine();
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Print()
+        {
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Programming-Advanced-C#/IteratorsAndComparators-Lab/Iterators/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Iterators
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> enumerable = new List<int>();
+
+            foreach (var item in enumerable)
+            {
+
+            }
+        }
+    }
+}
