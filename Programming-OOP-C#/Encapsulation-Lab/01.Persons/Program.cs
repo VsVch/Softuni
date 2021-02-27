f278488ea4beb1fe25d621901a2b446ab8338827
Sand82 <sandoki@abv.bg>
2021-02-27 22:33:26 +0200

    add new exercise

---
+++ Programming-OOP-C#/Encapsulation-Lab/01.Persons/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Person> persons = new List<Person>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string firstName = input[0];
+                string lastName = input[1];
+                int age = int.Parse(input[2]);
+
+                Person person = new Person(firstName, lastName, age);
+
+                persons.Add(person);
+
+            }
+
+            persons.OrderBy(p => p.FirstName)
+                .ThenBy(p => p.Age)
+                .ToList()
+                .ForEach(p => Console.WriteLine(p.ToString()));
+
+            
+        }
+    }
+}
