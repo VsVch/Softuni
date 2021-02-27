9f9a5c4a22baf9bb486c355381134c51728689e5
Sand82 <sandoki@abv.bg>
2021-02-27 22:32:55 +0200

    add new exercise

---
+++ Programming-OOP-C#/Encapsulation-Lab/EncapsulationLecture/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using InternalModifierDemo;
+
+namespace EncapsulationLecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Kitchen kitchen = new Kitchen();
+
+            if (kitchen.MeatballsLeft)
+            {
+                kitchen.OrderMeatball();
+            }
+
+        }
+    }
+}
