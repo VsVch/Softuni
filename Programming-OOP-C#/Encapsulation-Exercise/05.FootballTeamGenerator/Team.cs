106af2d7457851f0ea83e5a7f7bc6b712174c7bc
Sand82 <sandoki@abv.bg>
2021-03-04 00:13:26 +0200

    add new exercise

---
+++ Programming-OOP-C#/Encapsulation-Exercise/05.FootballTeamGenerator/Team.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _05.FootballTeamGenerator
+{
+    public class Team
+    {
+        private string name;        
+        private Dictionary<string, Player> playersByName;
+
+        public Team(string name)
+        {
+            this.Name = name;
+            playersByName = new Dictionary<string, Player>();
+        }
+
+        public string Name
+        {
+            get => this.name;
+            private set
+            {
+                Validator.NameValidation
+                    (value, GlobalConstants.InvalideNameExeptionsMessege);
+                this.name = value;
+            }
+        }
+
+        public double Rating
+        {
+            get
+            {
+                if (playersByName.Count <= 0)
+                {
+                    return 0;
+                }
+                return this.playersByName
+                    .Values
+                    .Average(p => p.AveragePlayerSkillLevel);
+            }                
+
+        }
+        public void AddPlayer(Player player)
+        {
+            this.playersByName.Add(player.Name, player);        
+        }
+
+        public void RemovePlayer(string playerName)
+        {
+            if (!this.playersByName.ContainsKey(playerName))
+            {
+                throw new InvalidOperationException($"Player {playerName} is not in {this.Name} team.");
+            }     
+            
+            playersByName.Remove(playerName);
+        }
+
+        
+    }
+}
---
+++ Programming-OOP-C#/Encapsulation-Exercise/05.FootballTeamGenerator/Validator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _05.FootballTeamGenerator
+{
+    public static class Validator
+    {
+        public static void SkillValidation(int min, int max, int value, string messagException) 
+        {
+            if (value < min || value > max)
+            {
+                throw new ArgumentException(messagException);
+            }
+        
+        }
+
+        public static void NameValidation(string value, string messagException) 
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException(messagException);
+            }       
+        
+        }
+    }
+}
