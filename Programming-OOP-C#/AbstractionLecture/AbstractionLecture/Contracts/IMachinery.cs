3cbf682bece262f8c3ddf28e8ccad903b68b58b8
Sand82 <sandoki@abv.bg>
2021-03-05 12:09:38 +0200

    add new exercise

---
+++ Programming-OOP-C#/AbstractionLecture/AbstractionLecture/Contracts/IMachinery.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractionLecture.Contracts
+{
+    public interface IMachinery
+    {
+        public void ListAllMachines();
+
+        List<string> machines { get; set; }
+    }
+}
---
+++ Programming-OOP-C#/AbstractionLecture/AbstractionLecture/Contracts/IOrdereble.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractionLecture.Contracts
+{
+    public interface IOrdereble
+    {
+       public void OrderMeal(string name);
+    }
+}
