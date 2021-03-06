b28796bb2f7773918152e6bdfb87794a905dd3f6
Sand82 <sandoki@abv.bg>
2021-03-06 22:35:17 +0200

    add new exercise

---
+++ Programming-OOP-C#/InterfacesAndAbstraction-Exercise/05.BirthdayCelebrations/Citizen.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _05.BirthdayCelebrations
+{
+    public class Citizen : IBirthable
+    {
+        public Citizen(string name, int age, string id, string birthdate)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Id = id;
+            this.Birthdate = birthdate;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Id { get; set; }
+
+        public string Birthdate { get; set; }
+    }
+}
