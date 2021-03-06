bd281cabaa27fcb901802a52a676464e5bc48b38
Sand82 <sandoki@abv.bg>
2021-03-06 22:34:45 +0200

    add new exercise

---
+++ Programming-OOP-C#/InterfacesAndAbstraction-Exercise/04.BorderControl/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.BorderControl
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IIdentificable> visitors = new List<IIdentificable>();
+
+            IIdentificable visitor = null;
+
+            string input;
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string[] line = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string name = line[0];
+
+                if (line.Length == 3)
+                {
+                    int age = int.Parse(line[1]);
+                    string id = line[2];
+
+                    visitor = new Citizen(name, age, id);
+
+                    visitors.Add(visitor);
+                }
+                else if (line.Length == 2) 
+                {
+                    string id = line[1];
+
+                    visitor = new Robot(name, id);
+
+                    visitors.Add(visitor);
+                }                
+            }           
+
+            string secretNumber = Console.ReadLine();
+
+           List<IIdentificable>filteredVisitors = visitors
+                .Where(v => v.Id.EndsWith(secretNumber))
+                .ToList();
+
+            foreach (var item in filteredVisitors)
+            {
+                Console.WriteLine(item.Id);
+            }
+        }
+    }
+}
