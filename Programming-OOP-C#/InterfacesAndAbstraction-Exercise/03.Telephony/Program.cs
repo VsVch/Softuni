3284c99716c8e508746b543646756fc4cc0fd4ed
Sand82 <sandoki@abv.bg>
2021-03-06 22:34:12 +0200

    add new exercise

---
+++ Programming-OOP-C#/InterfacesAndAbstraction-Exercise/03.Telephony/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03.Telephony
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Smartphone smartphone = new Smartphone();
+            StationaryPhone stationaryPhone = new StationaryPhone();
+
+            string[] phones = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string[] url = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+               
+           foreach (var phone in phones)
+           {         
+              try
+              {
+                  string result = phone.Length == 10 ?
+                  smartphone.Call(phone) : stationaryPhone.Call(phone);
+                  Console.WriteLine(result);
+              }
+              catch (ArgumentException ex)
+              {
+         
+                  Console.WriteLine(ex.Message);
+              }
+         
+           }
+         
+           foreach (var item in url)
+           {
+              
+              try
+              {
+                    string result = smartphone.Brows(item);
+                    Console.WriteLine(result);
+              }
+              catch (ArgumentException ex)
+              {
+
+                   Console.WriteLine(ex.Message);
+              }                
+           } 
+                  
+        }
+    }
+}
---
+++ Programming-OOP-C#/InterfacesAndAbstraction-Exercise/03.Telephony/StationaryPhone.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.Telephony
+{
+    public class StationaryPhone : ICallable
+    {
+       
+        public string Call(string number)
+        {
+            Validator.ThrowIfNumberIsInvalide(number);
+
+            return $"Dialing... {number}";
+        }
+    }
+}
