2fbedd3668d891049a552b8f59e8ba8c2272a9d4
Sand82 <sandoki@abv.bg>
2021-03-07 13:01:58 +0200

    add new exercise

---
+++ Programming-OOP-C#/Inheritance-Lab/05.StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings<T> : Stack<T>
+    {
+        public bool IsEmpty()
+        {
+            //bool isEmptyStack = true;
+
+            //if (this.Count > 0)
+            //{
+            //    isEmptyStack = false;
+            //}
+            //return isEmptyStack;
+
+            return this.Count == 0;
+        
+        }
+        public Stack<T> AddRange(IEnumerable<T> range)
+        {
+            foreach (var item in range)
+            {
+                this.Push(item);
+            }
+            return this;
+            
+        }
+    }
+}
---
+++ Programming-OOP-C#/Inheritance-Lab/05.StackOfStrings/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings<string> stack = new StackOfStrings<string>();
+
+            stack.AddRange(new List<string> 
+            { 
+                "Mishi",
+                "Lubi",
+                "Mimi",
+                "Sashi",            
+            });
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
