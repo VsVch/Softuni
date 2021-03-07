e57b5b1fe84bb8e8b5d9d656ee2d5afb4ba867dc
Sand82 <sandoki@abv.bg>
2021-03-07 12:59:50 +0200

    add new exercise

---
+++ Programming-OOP-C#/Inheritance-Exercise/Animals/StartUp.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace Animals
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string input;
+
+            Animal animal = null;
+
+            while ((input = Console.ReadLine()) != "Beast!")
+            {                
+                string[] rowDate = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);                
+
+                try
+                {
+                    string name = rowDate[0];
+                    int age = int.Parse(rowDate[1]);
+                    string gender = rowDate[2];                    
+
+                    if (input == nameof(Cat))
+                    {
+                        animal = new Cat(name, age, gender);                    
+                    }
+                    else if (input == nameof(Dog))
+                    {
+                        animal = new Dog(name, age, gender);                        
+                    }
+                    else if (input == nameof(Frog))
+                    {
+                        animal = new Frog(name, age, gender);                        
+                    }
+                    else if (input == nameof(Kitten))
+                    {
+                        animal = new Kitten(name, age);                        
+                    }
+                    else if (input == nameof(Tomcat))
+                    {
+                        animal = new Tomcat(name, age);                        
+                    }
+                    Console.WriteLine(input);
+                    Console.WriteLine(animal.ToString());
+                    Console.WriteLine(animal.ProduceSound());
+                }
+                catch (ArgumentException ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                   
+                   
+                }       
+           
+            }
+        }
+    }
+}
