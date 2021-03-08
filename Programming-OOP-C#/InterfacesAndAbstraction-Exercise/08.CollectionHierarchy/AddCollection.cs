4a0235a5cfe39ed23ab973f46ec1031e843c8cb7
Sand82 <sandoki@abv.bg>
2021-03-08 22:30:02 +0200

    add new exercise

---
+++ Programming-OOP-C#/InterfacesAndAbstraction-Exercise/08.CollectionHierarchy/AddCollection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _08.CollectionHierarchy
+{
+    public class AddCollection : IAddColection
+    {
+        private List<string> colection;
+
+        public AddCollection()
+        {
+            colection = new List<string>();
+        }
+        public IReadOnlyCollection<string> Colection => colection.AsReadOnly();
+
+        public int AddColection(string element)
+        {
+            colection.Insert(colection.Count, element);
+
+            return colection.Count - 1;
+        }
+    }
+}
---
+++ Programming-OOP-C#/InterfacesAndAbstraction-Exercise/08.CollectionHierarchy/AddRemoveCollection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _08.CollectionHierarchy
+{
+    public class AddRemoveCollection : IAddColection, IRemoveColection
+    {
+        private List<string> colection;
+
+        public AddRemoveCollection()
+        {
+            colection = new List<string>();
+        }
+
+        public IReadOnlyCollection<string> Colection => colection.AsReadOnly();
+
+        public int AddColection(string element)
+        {
+            int index = 0;
+            colection.Insert(0, element);
+
+            return index;
+        }
+
+        public string RemoveColection()
+        {
+            string element = colection[colection.Count - 1];
+            colection.RemoveAt(colection.Count - 1);
+
+            return element;
+        }
+    }
+}
