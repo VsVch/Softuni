f63ac14703a0d65541cbcdc8b6f34eb272f8188f
Sand82 <sandoki@abv.bg>
2021-03-09 22:37:14 +0200

    add new practice

---
+++ Programming-OOP-C#/Polymorphism-Exercise/01.Vehicles/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.Vehicles
+{
+    public class Car : Vehicle
+    {
+        private const double CarAirConditionConsumption = 0.9;
+        public Car(double fuel, double fuelConsumption) 
+            : base(fuel, fuelConsumption, CarAirConditionConsumption)
+        {
+        }
+       
+    }
+}
