ef6ec57e3521288db2021575cc1d07c52433ab51
Sand82 <sandoki@abv.bg>
2021-03-09 22:38:58 +0200

    add new exercise

---
+++ Programming-OOP-C#/Inheritance-Lab/AccessModifiers/Child.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccessModifiers
+{
+    public class Child : Base
+    {
+        public Child()
+        {
+            this.protectedField = 5;
+            
+        }
+
+        public double Weight { get; set; }
+
+        public override void BaseMethod() 
+        { 
+            Console.WriteLine("Im the child");        
+        }
+    }
+}
---
+++ Programming-OOP-C#/Inheritance-Lab/AccessModifiers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AccessModifiers
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           Base baseObject = new Base();
+
+            Child child = new Child();
+
+            baseObject.BaseMethod();
+            child.BaseMethod();
+                
+        }
+    }
+}
