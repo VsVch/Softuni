a802c6e07a35afd014dc2deec783f754c589ebb4
Sand82 <sandoki@abv.bg>
2021-03-11 22:08:12 +0200

    add new exercise

---
+++ Programming-OOP-C#/Polymorphism-Exercise/03.Raiding/BaseHero.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.Raiding
+{
+    public abstract class BaseHero
+    {
+        public BaseHero( string name, int power)
+        {           
+            this.Name = name;
+            this.Power = power;
+        }
+               
+        public string Name { get; set; }
+
+        public int Power { get; set; }
+
+        public virtual string CastAbility() 
+        {
+            return $"{GetType().Name} - {this.Name} healed for {this.Power}";        
+        }
+    }
+}
