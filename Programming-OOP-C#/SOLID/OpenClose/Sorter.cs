450347c1bcc2efa8d61f755a688e3fa9838423c0
Sand82 <sandoki@abv.bg>
2021-03-14 22:20:37 +0200

    add new exercise

---
+++ Programming-OOP-C#/SOLID/OpenClose/Sorter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenClose
+{
+    public class Sorter
+    {
+        private ISortingStrategy sortingStrategy;
+        public Sorter(ISortingStrategy sortingStrategy)
+        {
+            this.sortingStrategy = sortingStrategy;
+        }
+
+        public List<int> Sort(List<int> list) 
+        {
+            return sortingStrategy.Sort(list);
+        
+        }
+    }
+}
