58cf9ab9325186e62ea87402af1841cc506569a3
Sand82 <sandoki@abv.bg>
2021-03-14 19:22:04 +0200

    add new exercise

---
+++ Programming-OOP-C#/SOLID/Books/BookLocation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Books
+{
+    public class BookLocation
+    {
+        public Book Book { get; set; }
+        public int Location { get; set; }
+
+        public int TurnPage(Book book)
+        {
+            Location ++;
+
+            return Location;
+        }
+    }
+}
---
+++ Programming-OOP-C#/SOLID/Books/Liberaly.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Books
+{
+    public class Liberaly
+    {
+        private List<BookLocation> bookLocations;
+
+        public Liberaly()
+        {
+            bookLocations = new List<BookLocation>();
+        }
+
+        public void AddBook(Book book) 
+        {
+            bookLocations.Add(new BookLocation() { Book = book, Location = 0 });        
+        }
+
+        public int TurnPage(Book book)
+        {
+            BookLocation bookLocation = this.bookLocations.First(b => b.Book == book);
+            
+
+            return bookLocation.TurnPage(book);        
+        }
+    }
+}
