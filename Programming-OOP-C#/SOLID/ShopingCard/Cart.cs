85598c324ce1a508e1ee5f5636e8d30c98d4f94c
Sand82 <sandoki@abv.bg>
2021-03-15 09:50:00 +0200

    add new exercise

---
+++ Programming-OOP-C#/SOLID/ShopingCard/Cart.cs
@@ -0,0 +1,44 @@
+﻿using ShopingCard.Contracts;
+using ShopingCard.Rule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShopingCard
+{
+    public class Cart
+    {
+        private readonly List<OrderItem> items;
+        private readonly PriceCalculator pricecalculator =
+            new PriceCalculator();
+        public Cart()
+        {
+            this.items = new List<OrderItem>();
+        }
+
+        public IEnumerable<OrderItem> Items 
+        {
+            get { return new List<OrderItem>(this.items); }
+        }
+
+        public string CustomerEmail { get; set; }
+
+        public void Add (OrderItem orderItem)
+        {
+            this.items.Add(orderItem);
+        }
+        public decimal TotalAmound() 
+        {
+            decimal total = 0;
+
+            foreach (var item in this.items)
+            {
+                total += pricecalculator.CalculatePrice(item);
+            }
+
+            return total;
+        
+        }
+    }
+}
---
+++ Programming-OOP-C#/SOLID/ShopingCard/Rule/SpecialRule.cs
@@ -0,0 +1,25 @@
+﻿using ShopingCard.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopingCard.Rule
+{
+    public class SpecialRule : IPriceRule
+    {
+        public decimal CalculatePrice(OrderItem item)
+        {
+            decimal price = 0;
+            price +=item.Price * .4m;
+            decimal setsOfThree = item.Price / 3;
+            price -= setsOfThree * .2m;
+            
+            return price;
+        }
+
+        public bool IsMuch(OrderItem item)
+        {
+            return item.Sku.StartsWith("SPECIAL");
+        }
+    }
+}
