2daab9d42e60453446bcf4cf44922cc2842d742f
Sand82 <sandoki@abv.bg>
2021-03-18 20:50:09 +0200

    add new exercise

---
+++ Programming-OOP-C#/ReflectionAndAttributes-Lab/MultipleValueEnum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MultipleValueEnum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Days today =
+                Days.Tuesday | Days.Monday | 
+                Days.Wednesday | Days.Thursday;
+
+            Console.WriteLine((int)today);
+        }
+    }
+
+    [Flags]
+    enum Days 
+    { 
+        Monday = 1,
+        Tuesday = 2,
+        Wednesday = 4,
+        Thursday = 8
+    }
+}
