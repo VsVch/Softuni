b5eb28b760c94a43a1cb57367357a60c57a7b071
Sand82 <sandoki@abv.bg>
2021-03-18 20:44:57 +0200

    add new exercise

---
+++ Programming-OOP-C#/ReflectionAndAttributes-Lab/Reflection/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Reflection;
+using System.Text;
+
+namespace Reflection
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Type studentType = typeof(Student);
+
+            var interfaces = studentType.GetInterfaces();
+
+            foreach (var item in interfaces)
+            {
+                Console.WriteLine(item.Name);
+            }
+
+            MethodInfo[] methods = studentType.GetMethods();
+
+            foreach (var item in methods)
+            {
+                Console.WriteLine(item.Name);
+            }
+
+            //Console.WriteLine("Which class do you want to ispect");
+            //string className = Console.ReadLine();            
+            //Type stringBuilder = Type.GetType(className);
+            //Console.WriteLine(stringBuilder.AssemblyQualifiedName);
+            //Console.WriteLine(stringBuilder.FullName);
+            //Console.WriteLine(stringBuilder.Name);
+            //Console.WriteLine(stringBuilder.BaseType);
+
+
+
+        }
+    }
+}
