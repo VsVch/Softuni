9fedb27fbcfb3e809109c8ded48e5c00c51bbc35
Sand82 <sandoki@abv.bg>
2021-03-21 22:39:19 +0200

    add new exercise

---
+++ Programming-OOP-C#/UnitTesting-Lab/Bank.Tests/BankAccountTest.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Bank.Tests
+{
+    [TestFixture]
+    public class BankAccountTest
+    {
+        private decimal amount;
+        private string name;
+        private BankAccount bankAccount;
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.name = "Pesho";
+            this.amount = 5;
+            this.bankAccount = new BankAccount(amount, name);
+        }
+
+        [Test]
+        public void WhenAccountInitializeWhithPositiveValue_AmountShouldBeChenged() 
+        {      
+            Assert.That(bankAccount.Amount, Is.EqualTo(amount));       
+        }
+
+        [Test]
+        public void WhenAccountDepositmade_ShouldChangeAmount()
+        {
+            bankAccount.Deposit(amount);
+            Assert.That(bankAccount.Amount, Is.EqualTo(amount * 2));
+        }
+    }
+}
