fbcf5adf8b58e8d7b8a5faef20a92eb670d7a8e2
Sand82 <sandoki@abv.bg>
2021-03-22 22:16:17 +0200

    add new exercise

---
+++ Programming-OOP-C#/ReflectionAndAttributes-Exercise/CommandPattern/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using CommandPattern.Core;
+using CommandPattern.Core.Contracts;
+using System;
+
+namespace CommandPattern
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            //ICommandFactory commandFactory = new CommandFactory();
+
+            //ICommand command = commandFactory.CreateCommand("Hello");
+
+            //Console.WriteLine(command.Execute(new string[] { "Sasho" }));
+
+            ICommandInterpreter command = new CommandInterpreter();
+            IEngine engine = new Engine(command);
+            engine.Run();
+        }
+    }
+
+    
+}
