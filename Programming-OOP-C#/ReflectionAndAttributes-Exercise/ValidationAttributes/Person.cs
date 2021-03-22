523bac3c9dedfaee8f14d4c2a6a6846fbfcffa5c
Sand82 <sandoki@abv.bg>
2021-03-22 22:17:20 +0200

    add new exercise

---
+++ Programming-OOP-C#/ReflectionAndAttributes-Exercise/ValidationAttributes/Person.cs
@@ -0,0 +1,22 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+
+namespace ValidationAttributes
+{
+    public class Person
+    {
+        private const int minAge = 12;
+        private const int maxAge = 90;
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        [MyRequired]
+        public string Name { get; private set; }
+        [MyRange(minAge, maxAge)]
+        public int Age { get;private set; }
+    }
+}
