0de0bb7a0de47768d76334dfe0d8272efa174dbd
Sand82 <sandoki@abv.bg>
2021-04-15 09:09:38 +0300

    add new exercise

---
+++ Programming-OOP-C#/ReflectionAndAttributes-Lab/CreateInstance/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace CreateInstance
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Type sbType = typeof(StringBuilder);
+
+            StringBuilder sb = (StringBuilder)Activator.CreateInstance(sbType);
+            StringBuilder whitArguments =
+                (StringBuilder)Activator.CreateInstance(sbType, new object[] { "Hey hey" });
+            Console.WriteLine(whitArguments);
+
+            sb.Append("HELLO");
+            Console.WriteLine(sb);
+        }
+    }
+}
