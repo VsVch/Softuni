05eca24d6c73025f399c70f32586454bf2864de3
Sand82 <sandoki@abv.bg>
2021-04-15 09:04:05 +0300

    add new exercise

---
+++ Programming-OOP-C#/ReflectionAndAttributes-Lab/06.CodeTracker/TestClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuthorProblem
+{
+    [Author("Viktor")]
+    class TestClass
+    {
+        [Author("Gogicha")]
+        [Obsolete]
+        public void GofiMethod() 
+        { 
+
+        }
+
+        [Author("Dimitrichko")]
+        public void AlotOfWork() 
+        { 
+        }
+    }
+}
