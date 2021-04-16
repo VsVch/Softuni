5e4a4260da8b57541b3c45d94a30335158033e4c
Sand82 <sandoki@abv.bg>
2021-04-16 19:30:09 +0300

    add new exercise

---
+++ Programming-Advanced-C#/FunctionalProgramming-Lab/FunctionalProgrammingCycle/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FunctionalProgrammingCycle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Loop(() =>
+            {
+                Console.WriteLine("Misho, Lubi");
+            }, 10);
+        }
+
+        public static void Loop(Action work, int n)
+        {
+            if (n == 0)
+            {
+                return;
+            }
+            work();
+            Loop(work, n-1);
+        }
+    }
+}
