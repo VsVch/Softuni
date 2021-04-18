3c130dc515e789ca7a28c624d2cb2a2ae3ea4f4b
Sand82 <sandoki@abv.bg>
2021-04-18 23:36:30 +0300

    add new exercise

---
+++ HackerRank/ArrayManipulation/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        int n = arr[0];
+        int m = arr[1];
+
+        var numbers = new int[n + 1];
+
+        for (int i = 0; i < m; i++)
+        {
+            arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int a = arr[0];
+            int b = arr[1];
+            int k = arr[2];
+
+            numbers[a - 1] += k;
+            numbers[b] -= k;
+        }
+
+        long sum = 0;
+        long max = 0;
+
+        foreach (var num in numbers)
+        {
+            sum += num;
+            if (max < sum)
+                max = sum;
+        }
+
+        Console.WriteLine(max);
+    }
+}
