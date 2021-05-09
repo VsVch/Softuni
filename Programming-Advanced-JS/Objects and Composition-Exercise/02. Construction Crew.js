4e1f4e66378c990dfdad437acedc92277b1891cf
Sand82 <sandoki@abv.bg>
2021-05-09 23:42:34 +0300

    add new exercise

---
+++ Programming-Advanced-JS/Objects and Composition-Exercise/02. Construction Crew.js
@@ -0,0 +1,17 @@
+function solution(worker){
+
+    if(worker.dizziness){
+        worker.levelOfHydrated += worker.weight * 0.1 * worker.experience; 
+        worker.dizziness = false;
+    }
+
+    return worker;
+
+}
+
+console.log(solution({
+    weight: 80,
+    experience: 1,
+    levelOfHydrated: 0,
+    dizziness: true }
+  ));
