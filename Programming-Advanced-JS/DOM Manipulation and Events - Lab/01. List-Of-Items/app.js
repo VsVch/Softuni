c9f465664bd06b14cd7ab781dcec22260a50415b
Sand82 <sandoki@abv.bg>
2021-05-20 06:37:02 +0300

    add new exercise

---
+++ Programming-Advanced-JS/DOM Manipulation and Events - Lab/01. List-Of-Items/app.js
@@ -0,0 +1,10 @@
+function addItem() {
+
+    const liElement = document.createElement('li');
+
+    liElement.textContent = document.getElementById('newItemText').value;
+
+    document.getElementById('items').appendChild(liElement);
+
+    document.getElementById('newItemText').value = '';
+}
