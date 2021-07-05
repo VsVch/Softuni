90592ac49aa3a94114bdf0abbedef356f1f6f48a
Sand82 <sandoki@abv.bg>
2021-07-05 22:59:54 +0300

    add new main folder plus exercise

---
+++ Programming-Applications-JS/Asynchronous Programming/01.XHR/scripts/app.js
@@ -0,0 +1,13 @@
+function loadRepos() {
+  
+   let baseURL = 'https://api.github.com/users/testnakov/repos';
+   const httpRequest = new XMLHttpRequest();
+
+   httpRequest.addEventListener('readystatechange', function () {
+      if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+         document.getElementById('res').textContent = httpRequest.responseText;
+      }
+   });
+   httpRequest.open('GET', baseURL);
+   httpRequest.send();
+}
