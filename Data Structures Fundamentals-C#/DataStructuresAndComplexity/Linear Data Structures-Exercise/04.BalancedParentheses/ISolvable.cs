695b22cfd78236b08ebc1b1f7a377717d9a9b8a4
Sand82 <sandoki@abv.bg>
2021-08-22 18:11:59 +0300

    add new exercise

---
+++ Data Structures Fundamentals-C#/DataStructuresAndComplexity/Linear Data Structures-Exercise/04.BalancedParentheses/ISolvable.cs
@@ -0,0 +1,16 @@
+<<<<<<< HEAD
+﻿namespace Problem04.BalancedParentheses
+{
+    public interface ISolvable
+    {
+        bool AreBalanced(string parentheses);
+    }
+=======
+﻿namespace Problem04.BalancedParentheses
+{
+    public interface ISolvable
+    {
+        bool AreBalanced(string parentheses);
+    }
+>>>>>>> 0648223807a776df15beca43e3c4c62e18f62090
+}
