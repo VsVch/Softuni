b1128c2429dd8253a0ddb3ea1212660510509b5f
Sand82 <sandoki@abv.bg>
2021-08-22 18:01:56 +0300

    add new exercise

---
+++ Data Structures Fundamentals-C#/Linear Data Structures-Exercise/01.FasterQueue/Node.cs
@@ -0,0 +1,20 @@
+﻿namespace Problem01.FasterQueue
+{
+    public class Node<T>
+    {
+        public Node()
+        {
+
+        }
+
+        public Node(T item)
+        {
+            this.Item = item;
+        }
+
+        public T Item { get; set; }
+
+        public Node<T> Next { get; set; }
+        
+    }
+}
---
+++ Data Structures Fundamentals-C#/LinearDataStructures/ImplementStack/CoolStack.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace ImplementStack
+{
+    public class CoolStack<T>
+    {
+        private LinkedList<T> linkedList;
+
+        public CoolStack()
+        {
+            linkedList = new LinkedList<T>();
+        }
+
+        public int Count { get { return linkedList.Count; } }
+
+        public void Push(T element)
+        {
+            linkedList.Add(element);            
+        }
+
+        public Node<T> Peek()
+        {
+            return linkedList.Head;
+        }
+
+        public T Pop()
+        {
+            var element = linkedList.Remove().Value;           
+            return element;
+        }
+    }
+}
