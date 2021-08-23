b1b4b99b1397009c39f4b30e876169aea28be439
Aleksandar Stefanov <75274983+Sand82@users.noreply.github.com>
2021-08-23 20:43:38 +0300

    Update Program.cs

--- Data Structures Fundamentals-C#/Trees Representation and Traversal (BFS, DFS)-Lab/CoolTree/Program.cs
+++ Data Structures Fundamentals-C#/Trees Representation and Traversal (BFS, DFS)-Lab/CoolTree/Program.cs
@@ -29,34 +29,3 @@ namespace CoolTree
         }
     }
 }
-using System;
-using System.Collections.Generic;
-
-namespace CoolTree
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Node<int> node1 = new Node<int>(7,
-                new Node<int>(19,
-                     new Node<int>(1),
-                     new Node<int>(12),
-                     new Node<int>(31)),
-                new Node<int>(21),
-                new Node<int>(14,
-                     new Node<int>(23),
-                     new Node<int>(6))
-             );
-
-            Tree<int> tree = new Tree<int>();
-
-            tree.Root = node1;
-
-            List<Node<int>> list = tree.BFS(node1);
-            Console.WriteLine(String.Join(", ", list));
-
-            tree.DFS(node1, 0);
-        }
-    }
-}
