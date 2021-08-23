3f829fb024736210877646961e8db90630043581
Aleksandar Stefanov <75274983+Sand82@users.noreply.github.com>
2021-08-23 20:37:54 +0300

    add new exercice

---
+++ Data Structures Fundamentals-C#/Trees Representation and Traversal (BFS, DFS)-Lab/CoolTree/Node.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CoolTree
+{
+    public class Node<T>
+    {
+        public Node(T value, params Node<T>[] childrens)
+        {
+            Value = value;
+            Children = childrens.ToList();
+        }
+
+        public T Value { get; set; }
+
+        public List<Node<T>> Children { get; set; }
+
+
+        public override string ToString()
+        {
+            return Value.ToString();
+
+        }
+    }
+}
---
+++ Data Structures Fundamentals-C#/Trees Representation and Traversal (BFS, DFS)-Lab/CoolTree/Tree.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoolTree
+{
+    public class Tree<T>
+    {
+        public Node<T> Root { get; set; }
+
+        public void DFS(Node<T> node, int level)
+        {
+            Console.Write(new string(' ', level));
+            Console.WriteLine(node);
+
+            foreach (var child in node.Children)
+            {
+                DFS(child, level + 3);
+            }
+        }
+        public List<Node<T>> BFS(Node<T> root) 
+        {
+
+            Queue<Node<T>> queue = new Queue<Node<T>>();
+            List<Node<T>> list = new List<Node<T>>();
+
+            queue.Enqueue(root);
+
+            while (queue.Count != 0)
+            {
+                Node<T> node = queue.Dequeue();
+                list.Add(node);
+
+                foreach (var item in node.Children)
+                {
+                    queue.Enqueue(item);                    
+                }             
+            }
+            return list;
+        }
+    }
+}
