cfd1d1c6941766bad8d8143c7e67a42b4bce40fb
Aleksandar Stefanov <75274983+Sand82@users.noreply.github.com>
2021-08-31 06:43:13 +0300

    add new exercise

---
+++ Data Structures Fundamentals-C#/Heaps,BST-Lab/DataStructures/BinaryTree.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+   public class BinaryTree<T>
+    {
+        public BinaryTree(Node<T> root)
+        {
+            Root = root;
+        }
+        public Node<T> Root { get; set; }
+
+        public string DFSPreOrder(Node<T> node, int ident)
+        {
+            string result = $"{new string(' ', ident) }{node.Value}\n";
+
+            if (node.LeftChild != null)
+            {
+                result += DFSPreOrder(node.LeftChild, ident + 3);
+            }
+
+            if (node.RightChild != null)
+            {
+                result += DFSPreOrder(node.RightChild, ident + 3);
+            }
+
+            return result;
+        }
+
+        public string DFSInOrder(Node<T> node, int ident)
+        {
+            string result = string.Empty;
+
+            if (node.LeftChild != null)
+            {
+               result += DFSInOrder(node.LeftChild, ident + 3);
+            }
+
+            result += $"{new string(' ', ident) }{node.Value}\n";
+
+            if (node.RightChild != null)
+            {
+                result += DFSInOrder(node.RightChild, ident + 3);
+            }
+
+            return result;
+        }
+
+        public string DFSPostOrder(Node<T> node, int ident)
+        {
+            string result = string.Empty;
+
+            if (node.RightChild != null)
+            {
+                result += DFSPostOrder(node.LeftChild, ident + 3);
+            }
+
+            if (node.RightChild != null)
+            {
+                result += DFSPostOrder(node.RightChild, ident + 3);
+            }
+
+            result += $"{new string(' ', ident) }{node.Value}\n";
+
+            return result;
+        }
+    }
+}
---
+++ Data Structures Fundamentals-C#/Heaps,BST-Lab/DataStructures/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node<int> root = new Node<int>(1,
+                new Node<int>(5,
+                      new Node<int>(2),
+                      new Node<int>(3)),
+                new Node<int>(7,
+                       new Node<int>(9),
+                       new Node<int>(11))
+                );
+
+            BinaryTree<int> tree = new BinaryTree<int>(root);
+
+            Console.Write(tree.DFSPreOrder(tree.Root, 0));
+
+            Console.WriteLine("------------------");
+
+            Console.Write(tree.DFSInOrder(tree.Root, 0));
+
+            Console.WriteLine("------------------");
+
+            Console.Write(tree.DFSPostOrder(tree.Root, 0));
+        }
+    }
+}
