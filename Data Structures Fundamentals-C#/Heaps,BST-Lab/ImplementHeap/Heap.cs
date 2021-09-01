2e780e0fccce162982da29125207a8d72c0f413a
Aleksandar Stefanov <75274983+Sand82@users.noreply.github.com>
2021-09-01 18:21:19 +0300

    add new exercise

---
+++ Data Structures Fundamentals-C#/Heaps,BST-Lab/ImplementHeap/Heap.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImplementHeap
+{
+    //max Heap
+    public class Heap<T> where T : IComparable<T>
+    {
+        private List<T> heap;
+
+        public Heap()
+        {
+            heap = new List<T>();
+        }
+
+        public int Count { get { return heap.Count; } }
+
+        public T GetMax() 
+        {
+            return heap[0];
+        }
+
+        public void Add(T element) 
+        {
+            heap.Add(element);
+            HeapiFy(heap.Count - 1);
+        }
+
+        public void HeapiFy(int index)
+        {
+            if (index == 0)
+            {
+                return;
+            }
+
+            int parentIndex = (index - 1) / 2;
+
+            if (heap[index].CompareTo(heap[parentIndex]) > 0)
+            {
+                T holder = heap[parentIndex];
+                heap[parentIndex] = heap[index];
+                heap[index] = holder;
+                HeapiFy(parentIndex);            
+            }
+        }
+
+        public string DFSInOrder(int index, int indent) 
+        {
+            string result = "";
+            int leftChild = 2 * index + 1;
+            int rightChild = 2 * index + 2;
+
+            if (leftChild < heap.Count)
+            {
+                result += DFSInOrder(leftChild, indent + 3);
+            }
+
+            result += $"{new string(' ', indent) }{heap[index]}\n";
+
+            if (rightChild < heap.Count)
+            {
+                result += DFSInOrder(rightChild, indent + 3);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Data Structures Fundamentals-C#/Heaps,BST-Lab/ImplementHeap/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ImplementHeap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var integerHeap = new Heap<int>();
+            var elements = new List<int>() {4, 3, -5, 1, 15, 6, 9, 5, 25, 8, 17, 16 };
+            foreach (var el in elements)
+            {
+                integerHeap.Add(el);
+                Console.WriteLine(integerHeap.DFSInOrder(0, 0));
+                Console.WriteLine("---------------");
+            }           
+        }
+    }
+}
