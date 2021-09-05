a94fa9074a14fa3187ea9b3bbfb7c3ea233d7ace
Aleksandar Stefanov <75274983+Sand82@users.noreply.github.com>
2021-09-05 09:15:38 +0300

    add new exercise

---
+++ Data Structures Fundamentals-C#/Heaps,BST-Lab/02.MaxHeap/MaxHeap.cs
@@ -0,0 +1,47 @@
+﻿namespace _02.MaxHeap
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class MaxHeap<T> : IAbstractHeap<T>
+        where T : IComparable<T>
+    {
+        private List<T> heap;
+
+        public MaxHeap()
+        {
+            heap = new List<T>();
+        }
+
+        public int Size { get {return heap.Count; } }
+
+        public void Add(T element)
+        {
+            heap.Add(element);
+            HeapiFy(heap.Count - 1);
+        }
+
+        public T Peek()
+        {
+            return heap[0];
+        }
+
+        private void HeapiFy(int index) 
+        {
+            if (index == 0)
+            {
+                return;
+            }
+
+            int parent = (index - 1) / 2;
+
+            if (heap[index].CompareTo(heap[parent]) > 0)
+            {
+                T temp = heap[parent];
+                heap[parent] = heap[index];
+                heap[index] = temp;
+                HeapiFy(parent);
+            }
+        }
+    }
+}
