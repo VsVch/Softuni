ccba0aa46288cc2d94bd0ddda871864d15bef1c4
Aleksandar Stefanov <75274983+Sand82@users.noreply.github.com>
2021-09-13 23:26:14 +0300

    add new exercise

---
+++ Data Structures Fundamentals-C#/Heaps-and-BST-Exercise/01.BSTOpearations.Tests/02.LowestCommonAncestor/BinaryTree.cs
@@ -0,0 +1,84 @@
+﻿namespace _02.LowestCommonAncestor
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class BinaryTree<T> : IAbstractBinaryTree<T>
+        where T : IComparable<T>
+    {
+        public BinaryTree(
+            T value,
+            BinaryTree<T> leftChild,
+            BinaryTree<T> rightChild)
+        {
+            this.Value = value;
+            this.LeftChild = leftChild;
+            if (this.LeftChild != null)
+            {
+                this.LeftChild.Parent = this;
+            }
+            this.RightChild = rightChild;
+            if (this.RightChild != null)
+            {
+                this.RightChild.Parent = this;
+            }
+        }
+
+        public T Value { get; set; }
+
+        public BinaryTree<T> LeftChild { get; set; }
+
+        public BinaryTree<T> RightChild { get; set; }
+
+        public BinaryTree<T> Parent { get; set; }
+
+        public T FindLowestCommonAncestor(T first, T second)
+        {
+            var firsrNode = Search(first);
+            var secondNode = Search(second);
+
+            var firstList = GetAncestores(firsrNode);
+            var secondList = GetAncestores(secondNode);
+
+            var intersection = firstList.Intersect(secondList);
+            return intersection.ToArray()[0];
+        }
+
+        private List<T> GetAncestores(IAbstractBinaryTree<T> node ) 
+        {
+            var list = new List<T>();
+
+            while (node != null)
+            {
+                list.Add(node.Value);
+                node = node.Parent;
+            }
+
+            return list;
+        }
+
+        private IAbstractBinaryTree<T> Search(T element)
+        {
+            var node = this;
+
+            while (node != null)
+            {
+                if (element.CompareTo(node.Value) < 0)
+                {
+                    node = node.LeftChild;
+                }
+                else if (element.CompareTo(node.Value) > 0)
+                {
+                    node = node.RightChild;
+                }
+                else
+                {
+                    return node;
+                }
+            }
+
+            return null;
+        }
+    }
+}
