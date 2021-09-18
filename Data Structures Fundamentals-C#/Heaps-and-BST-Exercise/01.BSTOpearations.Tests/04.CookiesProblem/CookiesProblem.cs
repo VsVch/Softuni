e78fb0472dc1a25decf80437e251279793f9f5c0
Aleksandar Stefanov <75274983+Sand82@users.noreply.github.com>
2021-09-18 22:20:09 +0300

    Add files via upload

---
+++ Data Structures Fundamentals-C#/Heaps-and-BST-Exercise/01.BSTOpearations.Tests/04.CookiesProblem/CookiesProblem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Wintellect.PowerCollections;
+
+namespace _04.CookiesProblem
+{
+    public class CookiesProblem
+    {
+        public int Solve(int k, int[] cookies)
+        {
+            var bag = new OrderedBag<int>(cookies);
+            var smallesElement = bag.GetFirst();
+            var steps = 0;
+
+            while (smallesElement < k && bag.Count >= 2)
+            {
+                var firstSmallestCookie = bag.RemoveFirst();
+                var secondSmallestCookie = bag.RemoveFirst();
+
+                steps++;
+                bag.Add(firstSmallestCookie + (2 * secondSmallestCookie));
+                smallesElement = bag.GetFirst();
+            }
+
+            return smallesElement >= k ? steps : -1;
+        }
+    }
+}
