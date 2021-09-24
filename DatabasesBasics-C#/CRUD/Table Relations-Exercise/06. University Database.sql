bf0c7a1974a34f6e638c14bd8e38ccb85e55d0a8
Aleksandar Stefanov <75274983+Sand82@users.noreply.github.com>
2021-09-24 21:19:30 +0300

    Add files via upload

---
+++ DatabasesBasics-C#/CRUD/Table Relations-Exercise/06. University Database.sql
@@ -0,0 +1,35 @@
+CREATE TABLE Majors
+(
+	MajorID INT PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(50) NOT NULL
+)
+
+CREATE TABLE Students
+(
+	StudentID INT PRIMARY KEY IDENTITY,
+	StudentNumber INT NOT NULL,
+	StudentName VARCHAR(50),
+	MajorID INT FOREIGN KEY REFERENCES Majors(MajorID)
+)
+
+CREATE TABLE Payments
+(
+	PaymentID INT PRIMARY KEY IDENTITY,
+	PaymentDate DATETIME NOT NULL,
+	PaymentAmount DECIMAL(15,2) NOT NULL,
+	StudentID INT FOREIGN KEY REFERENCES Students(StudentID)
+)
+
+CREATE TABLE Subjects
+(
+	SubjectID INT PRIMARY KEY IDENTITY,
+	SubjectName VARCHAR(50) NOT NULL
+)
+
+CREATE TABLE Agenda
+(
+	StudentID INT FOREIGN KEY REFERENCES Students(StudentID),
+	SubjectID INT FOREIGN KEY REFERENCES Subjects(SubjectID),
+
+	CONSTRAINT PK_Students_Subjects PRIMARY KEY (StudentID,SubjectID)
+)
