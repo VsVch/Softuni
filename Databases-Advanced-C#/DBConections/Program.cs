52c3547fb4058e15c5209df83ac772efebed2b3d
Aleksandar Stefanov <75274983+Sand82@users.noreply.github.com>
2021-11-04 06:27:54 +0200

    add new exercise

---
+++ Databases-Advanced-C#/DBConections/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace DBConections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Server=localhost;Integrated Security=true;Database=SoftUni";
+
+            var conection = new SqlConnection(connectionString);
+            conection.Open();
+
+            conection.Close();
+        }
+    }
+}
