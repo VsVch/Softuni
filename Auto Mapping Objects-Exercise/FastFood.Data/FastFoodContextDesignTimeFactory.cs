3ba46aa5ee26464c46dc1ed2008f89ff681fc79f
Aleksandar Stefanov <75274983+Sand82@users.noreply.github.com>
2021-11-30 06:10:44 +0200

    add neew exercise

---
+++ Auto Mapping Objects-Exercise/FastFood.Data/FastFoodContextDesignTimeFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FastFood.Data
+{
+    public class FastFoodContextDesignTimeFactory : IDesignTimeDbContextFactory<FastFoodContext>
+    {
+        public FastFoodContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<FastFoodContext>();
+            builder.UseSqlServer("Server=localhost; User Id=sa;Password=@Stefanov820605; Database=FastFood");
+           
+            return new FastFoodContext(builder.Options);
+        }
+    }
+}
