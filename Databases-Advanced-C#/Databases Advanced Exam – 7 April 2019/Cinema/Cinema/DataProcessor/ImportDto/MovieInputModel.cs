5ec400d0e9bc8c0394f26d24e3e0a84d28309ea5
Aleksandar Stefanov <75274983+Sand82@users.noreply.github.com>
2021-12-22 23:28:06 +0200

    Add files via upload

---
+++ Databases-Advanced-C#/Databases Advanced Exam – 7 April 2019/Cinema/Cinema/DataProcessor/ImportDto/MovieInputModel.cs
@@ -0,0 +1,28 @@
+﻿using Cinema.Data.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Cinema.DataProcessor.ImportDto
+{
+    public class MovieInputModel
+    {
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
+        public string Title { get; set; }
+
+        [Range(0,9)]
+        public Genre Genre { get; set; }
+
+        public TimeSpan Duration { get; set; }
+
+        [Range(1, 10)]
+        public double Rating { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
+        public string Director { get; set; }
+    }
+}
+
