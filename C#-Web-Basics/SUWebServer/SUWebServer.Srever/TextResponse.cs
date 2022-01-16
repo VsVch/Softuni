1d7dc2c9c61c86c76337ec0aeda251ddb9b24487
Sand82 <sandoki@abv.bg>
2022-01-16 17:48:49 +0200

    add routing and async-await methods

---
+++ C#-Web-Basics/SUWebServer/SUWebServer.Srever/TextResponse.cs
@@ -0,0 +1,18 @@
+﻿using SUWebServer.Srever.HTTP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SUWebServer.Srever
+{
+    public class TextResponse : ContentResponse
+    {
+        public TextResponse(string text,
+            Action<Request, Response> PreRenderAction = null)
+            : base(text, ContentType.PlainText, PreRenderAction)
+        {
+        }
+    }
+}
