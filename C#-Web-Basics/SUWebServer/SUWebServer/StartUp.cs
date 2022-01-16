63637dfc454f088295f40ef110f36f3374170544
Sand82 <sandoki@abv.bg>
2022-01-16 20:07:06 +0200

    add namespace on assembly Startup

--- C#-Web-Basics/SUWebServer/SUWebServer/StartUp.cs
+++ C#-Web-Basics/SUWebServer/SUWebServer/StartUp.cs
@@ -2,90 +2,80 @@
 using SUWebServer.Srever.HTTP;
 using SUWebServer.Srever.Responses;
 using System.Net;
+using System;
 
-public class Startup
+namespace SUWebServer
 {
-    private const string HtmlForm = @"<form action='/HTML' method='POST'>
+    public class Startup
+    {
+        private const string HtmlForm = @"<form action='/HTML' method='POST'>
     Name: <input type='text' name ='Name'/>
     Age: <input type='number' name ='Age'/>
     <input type='submit' value ='Save'>
 </form>";
 
-    private const string DownloadForm = @"<form action='/Content' method='POST'>
+        private const string DownloadForm = @"<form action='/Content' method='POST'>
     <input type='submit' value ='Download Sites Content'/>    
 </form>";
 
-    private const string FileName = "content.txt";
+        private const string FileName = "content.txt";
 
-    static async Task Main()
-    {
-        await DownloadSiteAsTextFile(Startup.FileName,
+        static async Task Main()
+        {
+            await DownloadSiteAsTextFile(Startup.FileName,
             new string[] { "https://judge.softuni.org/", "https://softuni.org/" });
-        var server = new HttpServer(routes => routes
-            .MapGet("/", new TextResponse("Hello from the server!"))
-            .MapGet("/HTML", new TextResponse(Startup.HtmlForm))
-            .MapGet("/Redirect", new RedirectResponse("https://softuni.bg/org/"))
-            .MapPost("/HTML", new TextResponse("", Startup.AddFromDataAction))
-            .MapGet("/Content", new HtmlResponse(Startup.DownloadForm))
-            .MapPost("/Content", new TextFileResponse(Startup.FileName)));
-       await server.Start();
-    }
-
-
-
-    private static void AddFromDataAction(Request request, Response response)
-    {
-        response.Body = "";
-
-        foreach (var (key, value) in request.Form)
-        {
-            response.Body += $"{key} - {value}";
-            response.Body += Environment.NewLine;
+            var server = new HttpServer(routes => routes
+                .MapGet("/", new TextResponse("Hello from the server!"))
+                .MapGet("/HTML", new TextResponse(Startup.HtmlForm))
+                .MapGet("/Redirect", new RedirectResponse("https://softuni.org/"))
+                .MapPost("/HTML", new TextResponse("", Startup.AddFromDataAction))
+                .MapGet("/Content", new HtmlResponse(Startup.DownloadForm))
+                .MapPost("/Content", new TextFileResponse(Startup.FileName)));
+            await server.Start();
         }
-    }
 
-    private static async Task<string> DownloadWebSiteContent(string url)
-    {
-        var httpClient = new HttpClient();
-
-        using (httpClient)
+        private static void AddFromDataAction(Request request, Response response)
         {
-            var response = await httpClient.GetAsync(url);
+            response.Body = "";
 
-            var html = await response.Content.ReadAsStringAsync();
-
-            return html.Substring(0, 2000);
-
+            foreach (var (key, value) in request.Form)
+            {
+                response.Body += $"{key} - {value}";
+                response.Body += Environment.NewLine;
+            }
         }
-    }
 
-    private static async Task DownloadSiteAsTextFile(
-        string fileName, string[] urls)
-    {
-        var download = new List<Task<string>>();
-
-        foreach (var url in urls)
+        private static async Task<string> DownloadWebSiteContent(string url)
         {
-            download.Add(DownloadWebSiteContent(url));
+            var httpClient = new HttpClient();
+
+            using (httpClient)
+            {
+                var response = await httpClient.GetAsync(url);
+
+                var html = await response.Content.ReadAsStringAsync();
+
+                return html.Substring(0, 2000);
+
+            }
         }
-        var responses = await Task.WhenAll(download);
 
-        var responseString = string.Join(
-            Environment.NewLine + new String('-', 100),
-            responses);
+        private static async Task DownloadSiteAsTextFile(
+            string fileName, string[] urls)
+        {
+            var download = new List<Task<string>>();
 
-        await File.WriteAllTextAsync(fileName, responseString);
+            foreach (var url in urls)
+            {
+                download.Add(DownloadWebSiteContent(url));
+            }
+            var responses = await Task.WhenAll(download);
+
+            var responseString = string.Join(
+                Environment.NewLine + new String('-', 100),
+                responses);
+
+            await File.WriteAllTextAsync(fileName, responseString);
+        }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
