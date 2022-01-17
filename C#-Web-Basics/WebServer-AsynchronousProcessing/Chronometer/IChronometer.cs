6707235027963fa7d6f3a01faf9bde4d44d0a637
Sand82 <sandoki@abv.bg>
2022-01-17 19:28:28 +0200

    add new exercise

---
+++ C#-Web-Basics/WebServer-AsynchronousProcessing/Chronometer/IChronometer.cs
@@ -0,0 +1,18 @@
+﻿
+namespace MyChronometer
+{
+    public interface IChronometer
+    {
+        string GetTime { get; }
+
+        List<string> Laps { get; }
+
+        void Start();
+
+        void Stop();
+
+        string Lap();
+
+        void Reset();
+    }
+}
---
+++ C#-Web-Basics/WebServer-AsynchronousProcessing/Chronometer/StartUp.cs
@@ -0,0 +1,46 @@
+﻿using MyChronometer;
+
+IChronometer chronometer = new Chronometer();
+
+string line;
+
+while ((line = Console.ReadLine()) != "exit".ToLower())
+{
+    if (line == "start".ToLower())
+    {
+        Task.Run(() =>
+        {
+            chronometer.Start();
+        });
+    }
+    else if (line == "stop".ToLower())
+    {
+        chronometer.Stop();
+    }
+    else if (line == "lap".ToLower())
+    {
+        Console.WriteLine(chronometer.Lap());
+    }
+    else if (line == "laps".ToLower())
+    {
+        if (chronometer.Laps.Count == 0)
+        {
+            Console.WriteLine("Laps: no laps");
+            continue;
+        }
+        Console.WriteLine("Laps: ");
+
+        for (int i = 0; i < chronometer.Laps.Count; i++)
+        {
+            Console.WriteLine($"{i}, {chronometer.Laps[i]}");
+        }
+    }
+    else if (line == "reset".ToLower())
+    { 
+        chronometer.Reset();
+    }
+    else if (line == "time".ToLower())
+    {
+        Console.WriteLine(chronometer.GetTime);
+    }    
+}
