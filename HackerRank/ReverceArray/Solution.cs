5f0641483d3b8d624783af24d198a6eaae5e7f61
Sand82 <sandoki@abv.bg>
2022-02-23 22:33:34 +0200

    add new exercise

---
+++ HackerRank/ReverceArray/Solution.cs
@@ -0,0 +1,30 @@
+﻿
+public class Result
+{   
+    public static List<int> reverseArray(List<int> a)
+    {
+        var result = new List<int>();
+
+        for (int i = a.Count - 1; i >= 0; i--)
+        {
+            var num = a[i];
+            result.Add(num);
+        }
+       
+        return result;
+    }
+}
+
+public class Solution
+{
+    public static void Main(string[] args)
+    {     
+      
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').Select(int.Parse).ToList();
+
+        List<int> res = Result.reverseArray(arr);
+
+        Console.WriteLine(String.Join(" ", res));
+        
+    }
+}
