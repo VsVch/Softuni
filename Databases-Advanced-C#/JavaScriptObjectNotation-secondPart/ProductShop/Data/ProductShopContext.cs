ca80ba62296f4d83c4f39cb52bc5ed1ff4d4e96c
Aleksandar Stefanov <75274983+Sand82@users.noreply.github.com>
2022-05-23 09:14:13 +0300

    Update ProductShopContext.cs

--- Databases-Advanced-C#/JavaScriptObjectNotation-secondPart/ProductShop/Data/ProductShopContext.cs
+++ Databases-Advanced-C#/JavaScriptObjectNotation-secondPart/ProductShop/Data/ProductShopContext.cs
@@ -28,7 +28,7 @@
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseSqlServer(@"Server=localhost; User Id=sa;Password=@Stefanov820605;Database=ProductShop;");
+                optionsBuilder.UseSqlServer(@"Server=localhost; User Id=sa;Password=@Test123456;Database=ProductShop;");
                
             }
         }
