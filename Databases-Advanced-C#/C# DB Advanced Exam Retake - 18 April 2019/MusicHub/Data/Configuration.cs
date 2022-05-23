0476d1a61adefa7e94e5e184a5f5d57bd34e27c3
Aleksandar Stefanov <75274983+Sand82@users.noreply.github.com>
2022-05-23 08:57:48 +0300

    Update Configuration.cs

--- Databases-Advanced-C#/C# DB Advanced Exam Retake - 18 April 2019/MusicHub/Data/Configuration.cs
+++ Databases-Advanced-C#/C# DB Advanced Exam Retake - 18 April 2019/MusicHub/Data/Configuration.cs
@@ -3,6 +3,6 @@
    public static class Configuration
    {
         public static string ConnectionString =
-            @"Server=localhost; User Id=sa;Password=@Stefanov820605; Database=MusicHub";        
+            @"Server=localhost; User Id=sa;Password=@Test123456; Database=MusicHub";        
    }
 }
