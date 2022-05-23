05872d7bd29e3f10b61f67529cba5a7f3ea6a141
Aleksandar Stefanov <75274983+Sand82@users.noreply.github.com>
2022-05-23 09:09:53 +0300

    Update SoftUniContext.cs

--- Databases-Advanced-C#/EntityFrameworkIntroduction-Exercise/EntityFrameworkIntroduction/Data/SoftUniContext.cs
+++ Databases-Advanced-C#/EntityFrameworkIntroduction-Exercise/EntityFrameworkIntroduction/Data/SoftUniContext.cs
@@ -27,7 +27,7 @@ namespace EntityFrameworkIntroduction.Data.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=localhost;Database=SoftUni;User Id=sa;Password=@Stefanov820605;");
+                optionsBuilder.UseSqlServer("Server=localhost;Database=SoftUni;User Id=sa;Password=@Test123456;");
             }
         }
 
