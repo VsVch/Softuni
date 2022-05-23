af59e497d7ec92b99fcae50f847aa116ba45e01f
Aleksandar Stefanov <75274983+Sand82@users.noreply.github.com>
2022-05-23 09:14:54 +0300

    Update Configuration.cs

--- Databases-Advanced-C#/LINQ - exercise/MusicHub/Data/Configuration.cs
+++ Databases-Advanced-C#/LINQ - exercise/MusicHub/Data/Configuration.cs
@@ -3,8 +3,7 @@
    public static class Configuration
     {
         public static string ConnectionString =
-            @"Server=localhost;Database=MusicHub; User Id=sa;Password=@Stefanov820605";
-        //"Server=localhost; User Id=sa;Password=@Stefanov820605; Database=MusicHub"
-        //@"Server=.;Database=MusicHub;Trusted_Connection=True";
+            @"Server=localhost;Database=MusicHub; User Id=sa;Password=@Test123456";
+        
     }
 }
