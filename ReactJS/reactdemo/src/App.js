5003729262adec15dabaa0f070d08aaa85dac454
Sand82 <sandoki@abv.bg>
2022-06-29 17:43:58 +0300

    add new exercise

---
+++ ReactJS/reactdemo/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Footer from './Footer.js'
+import { Header } from './Header.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Header text="Hellow Earth" />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactJS/reactdemo/src/Header.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+
+export const Header = (props) => {
+  return (
+    <header className="App-header">
+      <h1>{props.text}</h1>
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>Hellow from Sand!!!</p>
+    </header>
+  );
+};
