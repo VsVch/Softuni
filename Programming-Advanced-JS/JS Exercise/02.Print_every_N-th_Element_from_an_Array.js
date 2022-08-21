346878e7ee626c20d90f20ff04c7614dbd5b4cc0
Sand82 <sandoki@abv.bg>
2022-08-21 21:33:54 +0300

    add new exercise

--- Programming-Advanced-JS/JS Exercise/02.Print_every_N-th_Element_from_an_Array.js
+++
@@ -1,9 +0,0 @@
-function solve(arr, step) {
-  return arr.filter((x, i) => i % step == 0 && x);
-}
-
-console.log(solve(["5", "20", "31", "4", "20"], 2));
-
-console.log(solve(["dsa", "asd", "test", "tset"], 2));
-
-console.log(solve(["1", "2", "3", "4", "5"], 6));
